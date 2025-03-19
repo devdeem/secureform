b7e3714168bc56e418746b8b87929ab4a28fb956
Alexandr Virgovič <91419390+devdeem@users.noreply.github.com>
2025-03-19 14:47:59 +0100

    register validateAllRoute only once in validationRoutes

--- src/routes/validate.route.ts
+++ src/routes/validate.route.ts
@@ -5,7 +5,7 @@ import validateAllRoute from "./validateAll.route";
 import { FastifyInstance } from "fastify";
 
 export default async function validationRoutes(fastify: FastifyInstance) {
+	fastify.register(validateAllRoute);
 	fastify.register(validateEmailRoute);
 	fastify.register(validatePasswordRoute);
-	fastify.register(validateAllRoute);
 }
